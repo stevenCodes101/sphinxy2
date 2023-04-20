5e191d35eda4a4fbdb6ef4c1920af0209ab71b20
Steven Ramirez <Steven_Ramirez@esri.com>
2023-04-20 14:49:58 -0600

    Use furo theme
    Add favicon

---
+++ src/sphinxy/conf.py
@@ -0,0 +1,12 @@
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+
+html_favicon = "_static/favicon.ico"
+
