3aa780bc5ac7ca7408ed984da92b6d05e8f923ca
Steven Ramirez <Steven_Ramirez@esri.com>
2023-04-20 15:04:37 -0600

    Add githubpages extension

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,12 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
